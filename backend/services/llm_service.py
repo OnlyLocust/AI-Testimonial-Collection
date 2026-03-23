1c5a3d17e1f7a8acd2ec21e192e8dc3fb9b0a5c0
Harsh Kr <harshchudasama4739s@gmail.com>
2026-03-23 18:34:40 +0530

    llm initialization

--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -1,6 +1,23 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_classic.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+API_KEY=os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", 
+    temperature=0.2,
+    google_api_key=API_KEY
+)
+
+
+
 def generate_first_question(business_prompt):
     # Replace with your first_question_chain
-    return f"What made you try this {business_prompt.split()[-1]}?"
+    return "What made you choose our product/service?"
 
 def generate_followup_question(business_prompt, history, last_answer):
     # Replace with your followup_chain
