1e675171dce2643fccdc9296d78f7c1edff9bd0b
Harsh Kr <harshchudasama4739s@gmail.com>
2026-03-24 19:09:42 +0530

    created sentiment service

--- backend/services/sentiment_service.py
+++ backend/services/sentiment_service.py
@@ -1,2 +1,16 @@
-def detect_sentiment():
-    pass
+from services.llm.llm_init import llm
+from services.prompts.sentiment import sentiment_prompt
+from langchain_classic.chains import LLMChain
+
+sentiment_chain = LLMChain(
+    llm=llm,
+    prompt=sentiment_prompt,
+    output_key="sentiment"
+)
+
+def detect_sentiment(user_answer, business_prompt):
+    sentiment_result = sentiment_chain.invoke({
+        "answer": user_answer,
+        "business_prompt": business_prompt
+    })
+    return sentiment_result["sentiment"].strip()
