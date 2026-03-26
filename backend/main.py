5a9380bb9bbd9faf318dad7cbc1001c8ca5cca6c
Harsh Kr <harshchudasama4739s@gmail.com>
2026-03-26 23:10:00 +0530

    added axios middleware into backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from routes import qa
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 app.include_router(qa.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
